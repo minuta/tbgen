b3acab25f6d45dba2eeee5d43b4d5bc8b39fee03
AF <afroehli@informatik.hu-berlin.de>
2013-10-26 17:33:09 +0200

    in tbgen.py : in Class Parser : parse(), get_fields(), is_negated(), neg_state()

--- tbgen.py
+++ tbgen.py
@@ -3,56 +3,44 @@ from pdb import set_trace
 
 
 class Parser(object):
-    """ Parses the given input file and creates a list of RawRules.
-    """
     def __init__(self, filename):
         self.filename = filename
 
     def parse(self):
-        """ Reads the contents of the given input file,
-            and returns a list of RawRules.
-        """
-
         file_lines = self.read_file()
-        line = file_lines[0]
-        self.get_fields(line)
+        splitted_rules = []
+        for line in file_lines:
+            splitted_rules.append(self.get_fields(line))
 
+        return splitted_rules
 
     def get_fields(self, rule_line):
-        return 0
-        ###fields = rule.split()
-        ###self.source_host = fields[0][1:]
-        ###self.dest_host = fields[1]
-        ###self.source_port = fields[2] + fields[3] + fields[4]
-        ###self.dest_port = fields[5] + fields[6] + fields[7]
-        ###self.protocol = fields[8]
-        ###self.action = action
-
+        fields = rule_line.split()
+        self.src_host = fields[0][1:]
+        self.dst_host = fields[1]
+        self.src_port = fields[2] + fields[3] + fields[4]
+        self.dst_port = fields[5] + fields[6] + fields[7]
+        self.protocol = fields[8]
+#         self.action = action
+        return self.src_host, self.dst_host, self.src_port,\
+               self.dst_port, self.protocol
 
     def read_file(self):
         with open(self.filename) as f:
             lines = f.readlines()
         return lines
 
+    def is_negated(self, field):
+        if field[0] == '!':
+            return True
+        return False
 
-# class RawRule(object):
-#     def __init__(self, src_net, src_net_negated, dst_net, dst_net_negated, ...):
-#         """ Saves input information in instance variables.
-#         """
-#         assert 0, "not implemented yet"
-# 
-#     def __str__(self):
-#         return self.raw_rule
-# 
-#     def __repr__(self):
-#         return self.raw_rule
-# 
-#     def normalize(self):
-#         """ Returns a list of Rule objects, which do not contain any negations.
-#         """
-#         assert 0, "not implemented yet"
-# 
-
+    def neg_state(self, rule_fields):
+        state = [False, False, False, False, False]
+        for i, field in enumerate(rule_fields):
+            if self.is_negated(field):
+                state[i] = True
+        return state
 
 # ------------------------ MAIN ---------------------------------------------
 def main():
@@ -62,7 +50,12 @@ def main():
 #         exit('Usage: %s <Firewall-Rule-Set-File>' % argv[0])
     filename = 'rules'
     a = Parser(filename)
+    raw_rules = a.parse()
+    print raw_rules[0]
+
+    print a.neg_state(raw_rules[0])
 
 __name__ == '__main__' and main()
+
 
 
