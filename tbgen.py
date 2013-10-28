9d96d03c526d3638d8f85e5b6cdb0a006e25aefb
AF <afroehli@informatik.hu-berlin.de>
2013-10-28 11:27:29 +0100

    Class IP_CALC is completely developed

--- tbgen.py
+++ tbgen.py
@@ -49,20 +49,40 @@ class Parser(object):
         return state
 
 
-class Abstract_Rule(object):
-    def port_to_interval(self, field):
-        low_port, high_port = field.split(':')
-        return Interval(low_port, high_port)
+class IP_CALC(object):
+    def __init__(self, subnet):
+        parts = subnet.split('/')
+        self.ip = parts[0]
+        self.mask = int(parts[1]) 
+        self.net_part = self.ip_to_num(self.ip) & self.long_mask()
 
     def ip_to_num(self, ip):
         b = ''.join([bin(int(x)+256)[3:] for x in ip.split('.')])
         return int(b, 2)
 
-    def get_subnet_minmax(self, subnet):
-        ip, mask = subnet.split('/')
-#         hostmin_bin = b[:-1] + '1'
-        return ip, mask
-        
+    def long_mask(self):
+        return (1 << 32) - (1 << 32 >> self.mask)
+
+    def get_subnet_minhost(self):
+        return self.net_part + 1
+
+    def get_subnet_maxhost(self):
+        host_bits = 32 - self.mask
+        max_host = 1 << host_bits
+        return self.net_part + max_host - 1
+
+    def num_to_bin(self, x):
+        return bin(x)[2:]
+
+    def num_to_ip(self, num):
+        return ".".join(map(lambda n: str(num>>n & 0xFF), [24,16,8,0]))
+
+
+class Abstract_Rule(object):
+    def port_to_interval(self, field):
+        low_port, high_port = field.split(':')
+        return Interval(low_port, high_port)
+
 
     def subnet_to_interval(self, field):
         pass
@@ -80,11 +100,63 @@ def main():
     print raw_rules[0]
 
     r = Abstract_Rule()
-    ip = '10.10.10.10/20'
-    print r.get_subnet_minmax(ip)
+    ip = '1.1.1.1/24'
 
+    
+    s = IP_CALC('1.1.1.1/24')
+    print s.num_to_ip(16843009)
+    print s.num_to_ip(4294967040L)
+
+#     a = s.get_subnet_minhost()
+#     b = s.get_subnet_maxhost()
+# 
+#     print a, b
+#     for i in range(a, b+1):
+#         print s.num_to_bin(i)
+# 
+#     m = r.get_subnet_min(ip)
+#     print r.int_to_bin(m)
+# 
 #     ip = '192.168.1.1'
 #     print ''.join([bin(int(x)+256)[3:] for x in ip.split('.')])
 
 __name__ == '__main__' and main()
+
+
+# --------------------- TESTS ----------------------------------------------
+def test_ipcalc_init():
+    s = IP_CALC('1.1.1.1/24')
+    assert s.ip == '1.1.1.1' and s.mask == 24
+
+def test_num_to_ip():
+    s = IP_CALC('1.1.1.1/24')
+    assert s.num_to_ip(16843009) == '1.1.1.1'
+    assert s.num_to_ip(4294967040L) == '255.255.255.0'
+
+def test_ip_to_num():
+    s = IP_CALC('1.1.1.1/24')
+    assert s.ip_to_num('1.1.1.1') == 16843009
+
+def test_mask():
+    s = IP_CALC('1.1.1.1/24')
+    assert s.long_mask() == 4294967040L
+
+def test_num_to_bin():
+    s = IP_CALC('1.1.1.1/24')
+    assert s.num_to_bin(16843009) == '1000000010000000100000001'
+    assert s.num_to_bin(4294967040L) =='11111111111111111111111100000000'
+
+def test_get_subnet_minhost():
+    s = IP_CALC('1.1.1.1/24')
+    assert s.num_to_ip(s.get_subnet_minhost()) == '1.1.1.1'
+
+    s = IP_CALC('11.12.13.14/7')
+    assert s.num_to_ip(s.get_subnet_minhost()) == '10.0.0.1'
+
+def test_get_subnet_maxhost():
+    s = IP_CALC('1.1.1.1/24')
+    assert s.num_to_ip(s.get_subnet_maxhost()) == '1.1.1.255'
+
+    s = IP_CALC('11.12.13.14/7')
+    assert s.num_to_ip(s.get_subnet_maxhost()) == '11.255.255.255'
 
