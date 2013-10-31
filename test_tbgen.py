f0ce10e38c7cd2e590eabd632aa71a3cea9b578d
AF <afroehli@informatik.hu-berlin.de>
2013-10-31 19:33:48 +0100

    add in test_tbgen.py : test_subnet_to_interval(), test_normalize()

--- test_tbgen.py
+++ test_tbgen.py
@@ -1,12 +1,14 @@
-from tbgen import (Parser)
+from tbgen import (Parser, RawRule)
+from interval import Interval
 
 import pytest
 skip = pytest.mark.skipif
 
+
 class TestParser():
 
-
     p = Parser('test_rules.txt')
+
     def test_check_negs(self):
 
         assert self.p.check_negs(['!192.151.11.17/32', '15.0.120.4/32',\
@@ -19,32 +21,42 @@ class TestParser():
                            '!10', ':', '655', '!1221', ':', '1221',\
                            '!0x06/0xff']) == [True, True, True, True, True]
 
-    def parse(self):
-        assert self.p.parse() 
 
-
-@skip
-def test_subnet_to_interval():
-    # check subnet '1.2.3.4/5'
-    assert subnet_to_interval(1, 2, 3, 4, 5) == Interval(0, 134217727)
-    # check subnet '5.6.7.8/0'
-    assert subnet_to_interval(5, 6, 7, 8, 0) == Interval(0, 2 ** 32 - 1)
-    assert subnet_to_interval(24, 102, 18, 97, 17) == Interval(409337856,
-            409370623)
-
-@skip
 class TestRawRule(object):
 
+    r = RawRule('', '', '', '', '', '', '', '', '', '', '', '')
+    def test_subnet_to_interval(self):
+        # check subnet '1.2.3.4/5'
+        f1 = ['1.2.3.4', '5']
+        assert self.r.subnet_to_interval(f1) == Interval(0, 134217727)
+        # check subnet '5.6.7.8/0'
+
+        f2 = ['5.6.7.8', '0']
+        assert self.r.subnet_to_interval(f2) == Interval(0, 2 ** 32 - 1)
+        
+        f3 = ['24.102.18.97', '17']
+        assert self.r.subnet_to_interval(f3) == Interval(409337856, 409370623)
+
     def test_normalize(self):
-        r1 = RawRule(Interval(1, 2), Interval(3, 4),
-                          Interval(5, 6), Interval(7, 8),
-                          Interval(9, 9), False, True, False,
-                          True, False, 1000)
+        action = 'PASS'
+        r_id = 13
+        m = 2 ** 32 - 1
+        i1 = Interval(1, 2)
+        i3 = Interval(5, 6)
+        i5 = Interval(9, 9)
+        r1 = RawRule(i1, Interval(3, 4),\
+                     i3, Interval(7, 8),
+                     i5, action,\
+                     False, True, False, True, False, r_id)
         rules = r1.normalize()
         assert len(rules) == 4
-#         assert rules[0] == ...
-#         assert rules[1] == ...
-#         ...
+
+        assert rules[0] == Rule(i1, Interval(0, 2), i3, Interval(0, 6), i5, action, r_id)
+        assert rules[1] == Rule(i1, Interval(0, 2), i3, Interval(9, m), i5, action, r_id)
+        assert rules[2] == Rule(i1, Interval(5, m), i3, Interval(0, 6), i5, action, r_id)
+        assert rules[3] == Rule(i1, Interval(5, m), i3, Interval(9, m), i5, action, r_id)
+
+        
 @skip        
 class TestRule(object):
 
